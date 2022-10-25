9ac62ee0489c32d17332ba8d7fa4209f938a1524
oosoriodr <113384970+oosoriodr@users.noreply.github.com>
2022-10-24 21:58:02 -0400

    Skeleton website created

---
+++ server/routes/employees.js
@@ -0,0 +1,59 @@
+// modules required for routing
+let express = require("express");
+let router = express.Router();
+let mongoose = require("mongoose");
+
+// define the employee model
+let employee = require("../models/employees");
+
+/* GET employee List page. READ */
+router.get("/", (req, res, next) => {
+  // find all employee in the employee_detail collection
+  employee.find((err, employees) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render("employees/index", {
+        title: "Emplyoees",
+        employees: employees,
+      });
+    }
+  });
+});
+
+//  GET the Employee Details page in order to add a new employee
+router.get("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// POST process the Employee Details page and create a new Employee - CREATE
+router.post("/add", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// GET the Employee Details page in order to edit an existing Employee
+router.get("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post("/:id", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// GET - process the delete by specific employeename
+router.get("/delete", (req, res, next) => {
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+module.exports = router;
